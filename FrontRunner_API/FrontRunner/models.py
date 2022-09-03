d802ba0b78e8f37c92ddb72214f23dc299a43e08
JacobBerkey <jacobberkey@yahoo.com>
2022-09-03 07:29:44 -0400

    Code Cleanup

--- FrontRunner_API/FrontRunner/models.py
+++ FrontRunner_API/FrontRunner/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-from django.core.validators import validate_slug, validate_email
 from .validators import validate_domainonly_email
 
 
